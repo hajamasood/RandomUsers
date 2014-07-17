31a1e72ddb009ffeaa9e2e945c27f63757244ff6
Haja Masood <hajamasood@hotmail.com>
2014-07-17 13:19:44 -0400

    Initial Commit

---
+++ RandomUsers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RandomUsers
+//
+//  Created by Haja Masood on 7/17/14.
+//  Copyright (c) 2014 Hajamasood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
